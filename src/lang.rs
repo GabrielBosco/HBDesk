c2f6addb9e61515036a0735a10e2e0fefff08cbe
Bosco <gabrieldbosco@gmail.com>
2024-11-12 09:14:51 -0300

    Update only PT-BR language

--- src/lang.rs
+++ src/lang.rs
@@ -1,92 +1,10 @@
 use hbb_common::regex::Regex;
 use std::ops::Deref;
 
-mod ar;
-mod be;
-mod bg;
-mod ca;
-mod cn;
-mod cs;
-mod da;
-mod de;
-mod el;
-mod en;
-mod eo;
-mod es;
-mod et;
-mod eu;
-mod fa;
-mod fr;
-mod he;
-mod hr;
-mod hu;
-mod id;
-mod it;
-mod ja;
-mod ko;
-mod kz;
-mod lt;
-mod lv;
-mod nb;
-mod nl;
-mod pl;
-mod ptbr;
-mod ro;
-mod ru;
-mod sk;
-mod sl;
-mod sq;
-mod sr;
-mod sv;
-mod th;
-mod tr;
-mod tw;
-mod uk;
-mod vn;
+mod ptbr; // Apenas o módulo PT-BR é necessário
 
 pub const LANGS: &[(&str, &str)] = &[
-    ("en", "English"),
-    ("it", "Italiano"),
-    ("fr", "Français"),
-    ("de", "Deutsch"),
-    ("nl", "Nederlands"),
-    ("nb", "Norsk bokmål"),
-    ("zh-cn", "简体中文"),
-    ("zh-tw", "繁體中文"),
-    ("pt", "Português"),
-    ("es", "Español"),
-    ("et", "Eesti keel"),
-    ("eu", "Euskara"),
-    ("hu", "Magyar"),
-    ("bg", "Български"),
-    ("be", "Беларуская"),
-    ("ru", "Русский"),
-    ("sk", "Slovenčina"),
-    ("id", "Indonesia"),
-    ("cs", "Čeština"),
-    ("da", "Dansk"),
-    ("eo", "Esperanto"),
-    ("tr", "Türkçe"),
-    ("vn", "Tiếng Việt"),
-    ("pl", "Polski"),
-    ("ja", "日本語"),
-    ("ko", "한국어"),
-    ("kz", "Қазақ"),
-    ("uk", "Українська"),
-    ("fa", "فارسی"),
-    ("ca", "Català"),
-    ("el", "Ελληνικά"),
-    ("sv", "Svenska"),
-    ("sq", "Shqip"),
-    ("sr", "Srpski"),
-    ("th", "ภาษาไทย"),
-    ("sl", "Slovenščina"),
-    ("ro", "Română"),
-    ("lt", "Lietuvių"),
-    ("lv", "Latviešu"),
-    ("ar", "العربية"),
-    ("he", "עברית"),
-    ("hr", "Hrvatski"),
+    ("pt", "Português (Brasil)"),
 ];
 
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
@@ -96,73 +14,9 @@ pub fn translate(name: String) -> String {
 }
 
 pub fn translate_locale(name: String, locale: &str) -> String {
-    let locale = locale.to_lowercase();
-    let mut lang = hbb_common::config::LocalConfig::get_option("lang").to_lowercase();
-    if lang.is_empty() {
-        // zh_CN on Linux, zh-Hans-CN on mac, zh_CN_#Hans on Android
-        if locale.starts_with("zh") {
-            lang = (if locale.contains("tw") {
-                "zh-tw"
-            } else {
-                "zh-cn"
-            })
-            .to_owned();
-        }
-    }
-    if lang.is_empty() {
-        lang = locale
-            .split("-")
-            .next()
-            .map(|x| x.split("_").next().unwrap_or_default())
-            .unwrap_or_default()
-            .to_owned();
-    }
-    let lang = lang.to_lowercase();
-    let m = match lang.as_str() {
-        "fr" => fr::T.deref(),
-        "zh-cn" => cn::T.deref(),
-        "it" => it::T.deref(),
-        "zh-tw" => tw::T.deref(),
-        "de" => de::T.deref(),
-        "nb" => nb::T.deref(),
-        "nl" => nl::T.deref(),
-        "es" => es::T.deref(),
-        "et" => et::T.deref(),
-        "eu" => eu::T.deref(),
-        "hu" => hu::T.deref(),
-        "ru" => ru::T.deref(),
-        "eo" => eo::T.deref(),
-        "id" => id::T.deref(),
-        "br" => ptbr::T.deref(),
-        "pt" => ptbr::T.deref(),
-        "tr" => tr::T.deref(),
-        "cs" => cs::T.deref(),
-        "da" => da::T.deref(),
-        "sk" => sk::T.deref(),
-        "vn" => vn::T.deref(),
-        "pl" => pl::T.deref(),
-        "ja" => ja::T.deref(),
-        "ko" => ko::T.deref(),
-        "kz" => kz::T.deref(),
-        "uk" => uk::T.deref(),
-        "fa" => fa::T.deref(),
-        "ca" => ca::T.deref(),
-        "el" => el::T.deref(),
-        "sv" => sv::T.deref(),
-        "sq" => sq::T.deref(),
-        "sr" => sr::T.deref(),
-        "th" => th::T.deref(),
-        "sl" => sl::T.deref(),
-        "ro" => ro::T.deref(),
-        "lt" => lt::T.deref(),
-        "lv" => lv::T.deref(),
-        "ar" => ar::T.deref(),
-        "bg" => bg::T.deref(),
-        "be" => be::T.deref(),
-        "he" => he::T.deref(),
-        "hr" => hr::T.deref(),
-        _ => en::T.deref(),
-    };
+    let lang = "pt".to_string();
+    let m = ptbr::T.deref();
+
     let (name, placeholder_value) = extract_placeholder(&name);
     let replace = |s: &&str| {
         let mut s = s.to_string();
@@ -179,27 +33,17 @@ pub fn translate_locale(name: String, locale: &str) -> String {
         }
         s
     };
+
     if let Some(v) = m.get(&name as &str) {
         if !v.is_empty() {
             return replace(v);
         }
     }
-    if lang != "en" {
-        if let Some(v) = en::T.get(&name as &str) {
-            if !v.is_empty() {
-                return replace(v);
-            }
-        }
-    }
+
     replace(&name.as_str())
 }
 
-// Matching pattern is {}
-// Write {value} in the UI and {} in the translation file
-//
-// Example:
-// Write in the UI: translate("There are {24} hours in a day")
-// Write in the translation file: ("There are {} hours in a day", "{} hours make up a day")
+// Função para extração de placeholders, mantém o mesmo comportamento
 fn extract_placeholder(input: &str) -> (String, Option<String>) {
     if let Ok(re) = Regex::new(r#"\{(.*?)\}"#) {
         if let Some(captures) = re.captures(input) {
@@ -224,12 +68,10 @@ mod test {
             ("{} sessions".to_string(), Some("3".to_string()))
         );
         assert_eq!(f(" } { "), (" } { ".to_string(), None));
-        // Allow empty value
         assert_eq!(
             f("{} sessions"),
             ("{} sessions".to_string(), Some("".to_string()))
         );
-        // Match only the first one
         assert_eq!(
             f("{2} times {4} makes {8}"),
             ("{} times {4} makes {8}".to_string(), Some("2".to_string()))
