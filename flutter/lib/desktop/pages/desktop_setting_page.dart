26c859ac4e53e0278c62db42d6c5f47865a22fb5
Bosco <gabrieldbosco@gmail.com>
2024-11-12 08:29:46 -0300

    Change statement, license

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1921,7 +1921,7 @@ class _AboutState extends State<_About> {
                             .marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com/privacy.html');
+                        launchUrlString('https://hbticorporativo.com.br');
                       },
                       child: Text(
                         translate('Privacy Statement'),
@@ -1929,7 +1929,7 @@ class _AboutState extends State<_About> {
                       ).marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com');
+                        launchUrlString('https://hbticorporativo.com.br');
                       },
                       child: Text(
                         translate('Website'),
@@ -1947,7 +1947,7 @@ class _AboutState extends State<_About> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                                'Copyright © ${DateTime.now().toString().substring(0, 4)} HB T.I Corp.\n$license',
                                 style: const TextStyle(color: Colors.white),
                               ),
                               Text(
